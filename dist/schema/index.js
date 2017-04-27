256f2e0d57b1328fbbb38bfe436da7cfe9b4aba1
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-04-27 23:08:50 +1200

    Fixed lint issues

--- dist/schema/index.js
+++ dist/schema/index.js
@@ -3,15 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.User = exports.BaseObject = undefined;
+exports.BaseObject = undefined;
 
 var _baseObject = require('./base-object');
 
-var _user = require('./user');
-
 exports.BaseObject = _baseObject.BaseObject;
-exports.User = _user.User;
 exports.default = {
-  BaseObject: _baseObject.BaseObject,
-  User: _user.User
+  BaseObject: _baseObject.BaseObject
 };
--- src/schema/index.js
+++ src/schema/index.js
@@ -1,16 +1,11 @@
 import {
   BaseObject,
 } from './base-object';
-import {
-  User,
-} from './user';
 
 export {
   BaseObject,
-  User,
 };
 
 export default {
   BaseObject,
-  User,
 };
