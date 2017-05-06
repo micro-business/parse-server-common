dacdd10c8fe0cab245a0b0ff7ad0f9672c6422a0
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-05-06 19:52:02 +1200

    Exposed PlatformType in index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import {
   BaseObject,
 } from './schema';
+import Platform from './platform';
 import ParseWrapperService from './parse-wrapper-service';
 import UserService from './user-service';
 
 export {
   BaseObject,
+  Platform,
   ParseWrapperService,
   UserService,
 };
 
 export default {
   BaseObject,
+  Platform,
   ParseWrapperService,
   UserService,
 };
