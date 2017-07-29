50d37964861cbabaa33e334b8eeb412ec9f3bad8
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-07-29 15:59:23 +1200

    Added dist directory back

---
+++ dist/services/__tests__/ParseWrapperService.test.js
@@ -0,0 +1,150 @@
+'use strict';
+
+var _immutable = require('immutable');
+
+var _v = require('uuid/v4');
+
+var _v2 = _interopRequireDefault(_v);
+
+require('../../../bootstrap');
+
+var _schema = require('../../schema');
+
+var _ = require('../');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var TestObject = function (_BaseObject) {
+  _inherits(TestObject, _BaseObject);
+
+  function TestObject(object) {
+    _classCallCheck(this, TestObject);
+
+    var _this = _possibleConstructorReturn(this, (TestObject.__proto__ || Object.getPrototypeOf(TestObject)).call(this, object, 'TestObject'));
+
+    _initialiseProps.call(_this);
+
+    return _this;
+  }
+
+  return TestObject;
+}(_schema.BaseObject);
+
+TestObject.spawn = function (info) {
+  var object = new TestObject();
+
+  TestObject.updateInfoInternal(object, info);
+
+  return object;
+};
+
+TestObject.updateInfoInternal = function (object, info) {
+  object.set('data', info.get('data'));
+};
+
+var _initialiseProps = function _initialiseProps() {
+  var _this2 = this;
+
+  this.updateInfo = function (info) {
+    var object = _this2.getObject();
+
+    TestObject.updateInfoInternal(object, info);
+
+    return _this2;
+  };
+
+  this.getInfo = function () {
+    return (0, _immutable.Map)({
+      id: _this2.getId(),
+      data: _this2.getObject().get('data')
+    });
+  };
+};
+
+describe('createQuery', function () {
+  test('should return the object when existing id provided.', _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+    var data, object, criteria, query, foundObjects;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            data = (0, _v2.default)();
+            _context.next = 3;
+            return TestObject.spawn((0, _immutable.Map)({ data: data })).save();
+
+          case 3:
+            object = _context.sent;
+            criteria = (0, _immutable.Map)({
+              fields: _immutable.List.of('data'),
+              id: object.getId()
+            });
+            query = _.ParseWrapperService.createQuery(TestObject, criteria);
+            _context.next = 8;
+            return query.find();
+
+          case 8:
+            foundObjects = _context.sent;
+
+
+            expect(foundObjects).toBeTruthy();
+            expect(foundObjects.length).toEqual(1);
+            expect(foundObjects[0].get('data')).toEqual(data);
+
+          case 12:
+          case 'end':
+            return _context.stop();
+        }
+      }
+    }, _callee, undefined);
+  })));
+
+  test('should return the object when existing ids provided.', _asyncToGenerator(regeneratorRuntime.mark(function _callee2() {
+    var data1, data2, object1, object2, criteria, query, foundObjects;
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            data1 = (0, _v2.default)();
+            data2 = (0, _v2.default)();
+            _context2.next = 4;
+            return TestObject.spawn((0, _immutable.Map)({ data: data1 })).save();
+
+          case 4:
+            object1 = _context2.sent;
+            _context2.next = 7;
+            return TestObject.spawn((0, _immutable.Map)({ data: data2 })).save();
+
+          case 7:
+            object2 = _context2.sent;
+            criteria = (0, _immutable.Map)({
+              ids: _immutable.List.of(object1.getId(), object2.getId())
+            });
+            query = _.ParseWrapperService.createQuery(TestObject, criteria);
+            _context2.next = 12;
+            return query.find();
+
+          case 12:
+            foundObjects = _context2.sent;
+
+
+            expect(foundObjects).toBeTruthy();
+            expect(foundObjects.length).toEqual(2);
+            expect(foundObjects[0].get('data')).toEqual(data1);
+            expect(foundObjects[1].get('data')).toEqual(data2);
+
+          case 17:
+          case 'end':
+            return _context2.stop();
+        }
+      }
+    }, _callee2, undefined);
+  })));
+});
---
+++ dist/services/__tests__/UserService.test.js
@@ -0,0 +1,204 @@
+'use strict';
+
+var _v = require('uuid/v4');
+
+var _v2 = _interopRequireDefault(_v);
+
+require('../../../bootstrap');
+
+var _ = require('../');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+describe('signUpWithUsernameAndPassword', function () {
+  test('should return the new signed up user', _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+    var username, emailAddress, result;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            username = (0, _v2.default)();
+            emailAddress = (0, _v2.default)() + '@email.com';
+            _context.next = 4;
+            return _.UserService.signUpWithUsernameAndPassword(username, (0, _v2.default)(), emailAddress);
+
+          case 4:
+            result = _context.sent;
+
+
+            expect(result.get('id')).toBeTruthy();
+            expect(result.get('username')).toBe(username);
+            expect(result.get('emailAddress')).toBe(emailAddress);
+            expect(result.get('emailVerified')).toBeFalsy();
+
+          case 9:
+          case 'end':
+            return _context.stop();
+        }
+      }
+    }, _callee, undefined);
+  })));
+});
+
+describe('signInWithUsernameAndPassword', function () {
+  test('should fail if email address does not exist', _asyncToGenerator(regeneratorRuntime.mark(function _callee2() {
+    var username;
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _context2.prev = 0;
+            username = (0, _v2.default)();
+            _context2.next = 4;
+            return _.UserService.signInWithUsernameAndPassword(username, (0, _v2.default)());
+
+          case 4:
+            fail('User signed in for email that does not exist. Email: ' + username);
+            _context2.next = 10;
+            break;
+
+          case 7:
+            _context2.prev = 7;
+            _context2.t0 = _context2['catch'](0);
+
+            expect(_context2.t0).toBeTruthy();
+
+          case 10:
+          case 'end':
+            return _context2.stop();
+        }
+      }
+    }, _callee2, undefined, [[0, 7]]);
+  })));
+
+  test('should fail if password is wrong', _asyncToGenerator(regeneratorRuntime.mark(function _callee3() {
+    var username;
+    return regeneratorRuntime.wrap(function _callee3$(_context3) {
+      while (1) {
+        switch (_context3.prev = _context3.next) {
+          case 0:
+            _context3.prev = 0;
+            username = (0, _v2.default)();
+            _context3.next = 4;
+            return _.UserService.signUpWithUsernameAndPassword(username, (0, _v2.default)(), (0, _v2.default)() + '@email.com');
+
+          case 4:
+            _context3.next = 6;
+            return _.UserService.signInWithUsernameAndPassword(username, (0, _v2.default)());
+
+          case 6:
+            fail('User signed in for incorrect password.');
+            _context3.next = 12;
+            break;
+
+          case 9:
+            _context3.prev = 9;
+            _context3.t0 = _context3['catch'](0);
+
+            expect(_context3.t0).toBeTruthy();
+
+          case 12:
+          case 'end':
+            return _context3.stop();
+        }
+      }
+    }, _callee3, undefined, [[0, 9]]);
+  })));
+
+  test('should return the signed in user', _asyncToGenerator(regeneratorRuntime.mark(function _callee4() {
+    var username, emailAddress, password, result;
+    return regeneratorRuntime.wrap(function _callee4$(_context4) {
+      while (1) {
+        switch (_context4.prev = _context4.next) {
+          case 0:
+            username = (0, _v2.default)();
+            emailAddress = (0, _v2.default)() + '@email.com';
+            password = (0, _v2.default)();
+            _context4.next = 5;
+            return _.UserService.signUpWithUsernameAndPassword(username, password, emailAddress);
+
+          case 5:
+            _context4.next = 7;
+            return _.UserService.signInWithUsernameAndPassword(username, password);
+
+          case 7:
+            result = _context4.sent;
+
+
+            expect(result.get('id')).toBeTruthy();
+            expect(result.get('username')).toBe(username);
+            expect(result.get('emailAddress')).toBe(emailAddress);
+            expect(result.get('emailVerified')).toBeFalsy();
+
+          case 12:
+          case 'end':
+            return _context4.stop();
+        }
+      }
+    }, _callee4, undefined);
+  })));
+});
+
+describe('getUserInfo', function () {
+  test('should reject if username does not exist', _asyncToGenerator(regeneratorRuntime.mark(function _callee5() {
+    var username;
+    return regeneratorRuntime.wrap(function _callee5$(_context5) {
+      while (1) {
+        switch (_context5.prev = _context5.next) {
+          case 0:
+            username = (0, _v2.default)();
+            _context5.prev = 1;
+            _context5.next = 4;
+            return _.UserService.getUserInfo(username);
+
+          case 4:
+
+            fail('Received user info for use that does not exist. Username: ' + username);
+            _context5.next = 10;
+            break;
+
+          case 7:
+            _context5.prev = 7;
+            _context5.t0 = _context5['catch'](1);
+
+            expect(_context5.t0.getErrorMessage()).toBe('No user found with username: ' + username);
+
+          case 10:
+          case 'end':
+            return _context5.stop();
+        }
+      }
+    }, _callee5, undefined, [[1, 7]]);
+  })));
+
+  test('should return the user info', _asyncToGenerator(regeneratorRuntime.mark(function _callee6() {
+    var username, result;
+    return regeneratorRuntime.wrap(function _callee6$(_context6) {
+      while (1) {
+        switch (_context6.prev = _context6.next) {
+          case 0:
+            username = (0, _v2.default)();
+            _context6.next = 3;
+            return _.UserService.signUpWithUsernameAndPassword(username, (0, _v2.default)(), (0, _v2.default)() + '@email.com');
+
+          case 3:
+            _context6.next = 5;
+            return _.UserService.getUserInfo(username);
+
+          case 5:
+            result = _context6.sent;
+
+
+            expect(result.get('id')).toBeTruthy();
+            expect(result.get('username')).toBe(username);
+
+          case 8:
+          case 'end':
+            return _context6.stop();
+        }
+      }
+    }, _callee6, undefined);
+  })));
+});
