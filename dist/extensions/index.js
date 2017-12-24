c530b155eeb05f95fff8a137ba9584d9a3f9c4e3
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-12-24 21:25:28 +1300

    Packages upgraded

---
+++ dist/extensions/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.NewSearchResultReceivedEvent = undefined;
+
+var _NewSearchResultReceivedEvent2 = require('./NewSearchResultReceivedEvent');
+
+var _NewSearchResultReceivedEvent3 = _interopRequireDefault(_NewSearchResultReceivedEvent2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.NewSearchResultReceivedEvent = _NewSearchResultReceivedEvent3.default;
